02484fb29386d04ffe95c3163ea1dd3c15881cca
Dobrowit <radekcz@gmail.com>
2024-11-01 22:47:22 +0100

    Aktualizacja

--- js/service-register.js
+++ js/service-register.js
@@ -9,3 +9,42 @@ if ('serviceWorker' in navigator) {
             });
     });
 }
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (event) => {
+    // Zapobiegamy wyświetleniu domyślnego monitu
+    event.preventDefault();
+    // Zapisujemy event, aby móc go wywołać później
+    deferredPrompt = event;
+
+    // Wyświetl przycisk instalacji
+    showInstallButton();
+});
+
+function showInstallButton() {
+    const installButton = document.getElementById('installButton');
+    installButton.style.display = 'block';
+
+    installButton.addEventListener('click', async () => {
+        installButton.style.display = 'none';
+        
+        if (deferredPrompt) {
+            deferredPrompt.prompt();  // Wyświetla monit o instalację
+
+            const { outcome } = await deferredPrompt.userChoice;
+            if (outcome === 'accepted') {
+                console.log('Użytkownik zaakceptował instalację.');
+            } else {
+                console.log('Użytkownik odrzucił instalację.');
+            }
+
+            deferredPrompt = null;  // Wyczyść deferredPrompt
+        }
+    });
+}
+
+window.addEventListener('appinstalled', () => {
+    console.log('Aplikacja została zainstalowana');
+    document.getElementById('installButton').style.display = 'none';
+});
