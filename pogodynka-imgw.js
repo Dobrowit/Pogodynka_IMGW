6625436fbacf6ac53be7bc1d5173a2740f2b06c5
Dobrowit <radekcz@gmail.com>
2024-11-01 17:36:17 +0100

    Aktualizacja

--- pogodynka-imgw.js
+++ pogodynka-imgw.js
@@ -129,7 +129,7 @@ function displayData(synopData, meteoData, hydroData, hydro2Data) {
             <div class="weather-value">${meteoData.wiatr_poryw_10min} m/s</div>
             <div class="weather-date">${meteoData.wiatr_poryw_10min_data}</div>
         </div>
-        ${meteoData.opad_10min !== 0 ? `
+        ${meteoData.opad_10min != null && meteoData.opad_10min !== 'null' ? `
         <div class="weather-item">
             <div class="weather-label">Opad 10 min:</div>
             <div class="weather-value">${meteoData.opad_10min} mm</div>
